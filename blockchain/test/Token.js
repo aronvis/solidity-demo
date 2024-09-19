5439a259aa7a7c08e448c3be3dcb0759a96b166c
Aron Vischjager <aron@vischjager.com>
2024-09-19 20:08:57 +0200

    Update to code

--- blockchain/test/Token.js
+++ blockchain/test/Token.js
@@ -1,29 +1,12 @@
-// We import Chai to use its asserting functions here.
 const { expect } = require("chai");
-
-// We use `loadFixture` to share common setups (or fixtures) between tests.
-// Using this simplifies your tests and makes them run faster, by taking
-// advantage or Hardhat Network's snapshot functionality.
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 
-// `describe` is a Mocha function that allows you to organize your tests.
-// Having your tests organized makes debugging them easier. All Mocha
-// functions are available in the global scope.
-//
-// `describe` receives the name of a section of your test suite, and a
-// callback. The callback must define the tests of that section. This callback
-// can't be an async function.
 describe("Token contract", function () {
   async function deployTokenFixture() {
     // Get the ContractFactory and Signers here.
     const Token = await ethers.getContractFactory("Token");
     const [owner, addr1, addr2] = await ethers.getSigners();
-
-    // To deploy our contract, we just have to call Token.deploy() and await
-    // for it to be deployed(), which happens onces its transaction has been
-    // mined.
     const hardhatToken = await Token.deploy();
-
     await hardhatToken.deployed();
 
     // Fixtures can return anything you consider useful for your tests
@@ -34,12 +17,6 @@ describe("Token contract", function () {
   describe("Deployment", function () {
     it("Should set the right owner", async function () {
       const { hardhatToken, owner } = await loadFixture(deployTokenFixture);
-
-      // Expect receives a value and wraps it in an assertion object. These
-      // objects have a lot of utility methods to assert values.
-
-      // This test expects the owner variable stored in the contract to be
-      // equal to our Signer's owner.
       expect(await hardhatToken.owner()).to.equal(owner.address);
     });
 
