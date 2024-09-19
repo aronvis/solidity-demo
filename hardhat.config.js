bebf25cbd28632f088d10b4b659fdc98659f7afc
Aron Vischjager <aron@vischjager.com>
2024-09-19 19:11:44 +0200

    Replace truffle with hardhat

---
+++ hardhat.config.js
@@ -0,0 +1,15 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.13",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: "./blockchain/contracts",
+  },
+};
