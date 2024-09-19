b0ab9b4c7d918adc7958d8cfc457e044342e8730
Aron Vischjager <aron@vischjager.com>
2024-09-19 14:11:50 +0200

    Update to code

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
+const Token = artifacts.require("Token");
 const EthSwap = artifacts.require("EthSwap");
 
 module.exports = function (deployer) {
+  // Deploy Token
+  deployer.deploy(Token);
+
+  // Deploy EthSwap
   deployer.deploy(EthSwap);
 };
