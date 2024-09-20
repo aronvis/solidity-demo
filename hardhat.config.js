6f05791e9ef160597146877f1b03cd5b358442a8
Aron Vischjager <aron@vischjager.com>
2024-09-20 18:48:11 +0200

    Update to hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,12 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "sepolia",
+  hardhat: {},
+  sepolia: {
+    url: process.env.ETH_SEPOLIA_NODE_URL,
+    accounts: `0x${process.env.PRIVATE_KEY}`,
+  },
   solidity: {
     version: "0.8.13",
     settings: {
